b3f9eedcbb2868e0e2af41fd144705814f5cb6b4
Vova Polishchuk <volodimirpolishchuck@gmail.com>
2025-10-15 20:53:44 +0300

    update 5.7: toggling detailed info

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,27 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
+
+const Blog = ({blog}) => {
+    const { title, author, url, likes } = blog
+    const [show, setShow] = useState(false)
+
+    const button = () =>
+        <button onClick={() => setShow(!show)}>
+            {show ? 'hide' : 'view'}
+        </button>
+
+    return (
+        <div className="blog">
+            <span>{title} {button()}</span>
+            {
+                show &&
+                <div>
+                    <div>{url}</div>
+                    <div><span>{likes} <button>like</button></span></div>
+                    <div><span>{author}</span></div>
+                </div>
+            }
+        </div>
+    )
+}
 
 export default Blog
